71b752af41aa5afba2766e06e505f2e7cb9a2c5a
Michael Cooper <mc454916@ohio.edu>
2020-05-25 21:37:40 -0400

    Relocate json usage
    Add errors for incomplete languages

--- Application.py
+++ Application.py
@@ -3,18 +3,23 @@ from tkinter import filedialog, Tk, Toplevel, messagebox
 import pprint
 import tkinter as tk
 from tkinter.ttk import Combobox
-from backendInteract import *
 import Pages
+import json
 
 WIDTH = 500
 HEIGHT = 500
+
+# Load these from JSON in the future with these as defaults?
+COMMFILE = "languageComments.json"
+REGEXFILE = "languageRegexes.json"
+TITLE = "Auto Doc"
 
 #   Application class
 class Application(tk.Frame):
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master, relief=tk.GROOVE)
-        master.title("Auto Doc")
+        master.title(TITLE)
         master.geometry("{}x{}".format(WIDTH, HEIGHT))
         master.resizable(width=False, height=False)
 
@@ -38,6 +43,7 @@ class Application(tk.Frame):
 
         for page in self.pages:
             page.place(in_=self.contentFrame, x=0, y=0, relwidth=1, relheight=1)
+            page.setApp(self)
 
         # # binds
         # # unbind if previously bound
@@ -59,11 +65,39 @@ class Application(tk.Frame):
         if self.SELECTED is None or self.selectedFiles is None:
             messagebox.showinfo("Error", "Please Select a Language and Files")
         else:
-            self.pages[1].postInit()  # init documentor object here
+            if self.checkLangInJSON():
+                self.pages[1].postInit()  # init documentor object here
+                self.pages[1].show()
 
-    def restart(self):
+    def checkLangInJSON(self):
+        with open('./json/languageComments.json', encoding='utf-8') as comInfo:
+            data1 = json.loads(comInfo.read())
+
+        if self.SELECTED in data1:
+            self.pages[1].setLangCom(data1[self.SELECTED])
+        else:
+            messagebox.showerror("Language not found", "Language: {} not found in {}".format(self.SELECTED, COMMFILE))
+            return False
+        
+        with open('./json/languageRegexes.json', encoding='utf-8') as regInfo:
+            data2 = json.loads(regInfo.read())
+
+        if self.SELECTED in data2:
+            rx = data2[self.SELECTED]
+            if rx != "":
+                self.pages[1].setLangRx(rx)
+            else:
+                messagebox.showerror("Illegal Regex", "Language: {} regex cannot be blank".format(self.SELECTED))
+                return False
+        else:
+            messagebox.showerror("Language not found", "Language: {} not found in {}".format(self.SELECTED, REGEXFILE))
+            return False
+
+        return True
+
+    def restart(self, files=None):
         self.SELECTED = None
-        self.selectedFiles = None
+        self.selectedFiles = files
         self.pages[0].restart()
         self.pages[0].show()
 
--- Documenter.py
+++ Documenter.py
@@ -4,30 +4,30 @@ from typing import List
 from os import rename
 
 import re
-import json
+
+from tkinter import messagebox
 
 class Documenter():
 
-	def __init__(self, language, files):
+	def __init__(self, language, files, rx, app):
 		self.LANG = language
 		self.FILES = files
 		self.CURFILE = files[0]
+		self.APP = app
 		self.curFileCounter = 0
-		self.langFuncRegex = None
+		self.langFuncRegex = rx
 		self.content = None
-		self.langCommTemplate = None
 
-		self.getRegex()
-		self.getCommentFormat()
+		# self.getRegex()
+		# self.getCommentFormat()
 
 	def nextFile(self):
 		self.curFileCounter += 1
 		if self.curFileCounter < len(self.FILES):
 			self.CURFILE = self.FILES[self.curFileCounter]
-			# self.getFileContent() # getfilecontent is called on getlines, which is always called from documentor page
 		else:
 			self.CURFILE = "NO MORE FILES"
-		print(self.CURFILE)
+		# print(self.CURFILE)
 
 	def findFuncDec(self, fileContent):
 		arrInds = []	#empty list
@@ -50,6 +50,7 @@ class Documenter():
 		# keys are now in reverse order to document from bottom down, keeping other keys simple with no extra math
 		for k in keys:		# begin looping over all keys (will be from bottom up)
 			commentsDict[k].reverse()
+			############## add logic here to see how many whitespace characters are on line below ##############
 			for c in commentsDict[k]:	# should work :)
 				self.content.insert(k, c)		# insert comments in reverse order so they appear correctly
 
@@ -58,30 +59,18 @@ class Documenter():
 		# 	fileContent.insert(index, line)
 		# return fileContent		# return new version of filecontent
 
-	def getRegex(self):
-		with open('languageRegexes.json', encoding='utf-8') as langInfo:
-			data = json.loads(langInfo.read())
+	# def getRegex(self):
+	# 	with open('./json/languageRegexes.json', encoding='utf-8') as langInfo:
+	# 		data = json.loads(langInfo.read())
 
-		regex = ""
-
-		regex = data[self.LANG]
-
-		self.langFuncRegex = regex
-
-	def getCommentFormat(self):
-		with open('languageComments.json', encoding='utf-8') as comInfo:
-			data = json.loads(comInfo.read())
-
-		comments = data[self.LANG]
-
-		self.langCommTemplate = comments
+	# 	self.langFuncRegex = data[self.LANG]
 
 	def getFileContent(self):
 		self.content = None
 		with open(self.CURFILE) as f:
 			self.content = f.readlines()
 		self.content = [tmp.strip('\n') for tmp in self.content]
-		print(self.content)
+		# print(self.content)
 
 	def getLines(self) -> dict:
 		self.getFileContent()
@@ -89,33 +78,14 @@ class Documenter():
 
 		linesDict = dict()
 		for line in lines:
-			print("Function Declared at line:", line)
+			# print("Function Declared at line:", line)
 			linesDict[line] = self.content[line]
 
 		# for key in linesDict:
 		# print(key, linesDict[key])
 		return linesDict
 
-	# What exactly was this doing again?
-	# update to use dict of lists of strings :)
 	def addComment(self, commentsDict) -> None:
-		self.getCommentFormat()
-		comContent = []
-		comChar = self.langCommTemplate[0]
-
-		# get list of comments (already in order from top of file to bottom)
-		# -> create dict where key=linenumber val=listitem
-		# -> reverse list -> create filecontent string with comments
-		# maybe try using generators in the future? https://youtu.be/6QyJVF4buE0
-
-		# keys = commentsDict.keys()
-		#
-		# keys.sort()
-		# keys.reverse()
-
-		# for key in keys:
-		# 	for line in comment[key]:
-		# 		comContent.append(comChar + line)
 
 		self.comment(commentsDict)
 
@@ -124,10 +94,9 @@ class Documenter():
 		f = open(self.CURFILE, 'w')
 
 		self.content = "\n".join(self.content)
-		if self.content != "machine broke error":
-			print(self.content)
+		if self.content is not None:
+			# print(self.content)
 			f.write(self.content)
-
 
 if __name__ == "__main__":
 	pass
